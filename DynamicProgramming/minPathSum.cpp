681bd45f47707dc42547c9c86d6085a5e39e59ca
aryanxwork <aryanhere10@gmail.com>
2026-08-24 22:23:15 +0530

    dp on subsequences start

--- DynamicProgramming/minPathSum.cpp
+++ DynamicProgramming/minPathSum.cpp
@@ -6,8 +6,12 @@ int minPathSum(vector<vector<int>> &grid)
     int m = grid.size();
     int n = grid[0].size();
 
-    vector<vector<int>> dp(m, vector<int>(n, 0));
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX)); // we have to take min(down,right) so initializing them with INT_MAX padding is perfect - but it can cause overflow because some grid value + int max = overflow
+    // but the constraints of the problems define grid values are smaller so its fine here, we can use long long to fix otherwise
     dp[m - 1][n - 1] = grid[m - 1][n - 1];
+    // dp[i][j]=min cost to reach the destination using the current cell
+    //  starting at destination (cost = grid destination) and ending at 0,0
+    //  dp[0][0] = ans
 
     for (int i = m - 1; i >= 0; i--)
     {
--- DynamicProgramming/uniquePaths.cpp
+++ DynamicProgramming/uniquePaths.cpp
@@ -18,6 +18,55 @@ int uniquePaths(int m, int n)
     }
     return dp[0][0]; // unique paths from start point to destination
 }
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Mark obstacles as -1
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (obstacleGrid[i][j] == 1)
+                    dp[i][j] = -1;
+            }
+        }
+
+        // Destination
+        if (dp[m - 1][n - 1] != -1)
+            dp[m - 1][n - 1] = 1;
+
+        // dp[i][j] = number of paths from (i, j) to destination
+
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                // Current cell is an obstacle
+                if (dp[i][j] == -1)
+                    continue;
+
+                // Destination already initialized
+                if (i == m - 1 && j == n - 1)
+                    continue;
+
+                if (dp[i + 1][j] != -1)
+                    dp[i][j] += dp[i + 1][j];
+
+                if (dp[i][j + 1] != -1)
+                    dp[i][j] += dp[i][j + 1];
+            }
+        }
+
+        return dp[0][0] == -1 ? 0 : dp[0][0];
+    }
+};
 int main()
 {
     cout << uniquePaths(3, 2);
